7c81d064b6b6ad75674cd130712fa882590d1598
neenukurian92@gmail.com <neenukurian92@gmail.com>
2025-10-27 16:23:56 +0100

    feat: add loading component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-16 h-16 border-4 border-blue-500 rounded-full animate-spinner bg-white mt-24 mx-auto"></div>
+    <p v-if="message">{{ message }}</p>
+</template>
+
+<script setup lang="ts">
+import type { AppLoaderProps } from '@/types/index.ts';
+
+defineProps<AppLoaderProps>();
+</script>
+
+<style scoped>
+@keyframes spinner {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
