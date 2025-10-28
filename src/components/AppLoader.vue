819d1b350cbaf0f6f24a91c2c3f06676853a5b9e
neenukurian92@gmail.com <neenukurian92@gmail.com>
2025-10-28 21:27:42 +0100

    fix: color of loader

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-16 h-16 border-4 border-blue-500 rounded-full animate-spinner bg-white mt-24 mx-auto"
+    class="w-16 h-16 border-4 border-gray-400 rounded-full animate-spinner bg-white mt-24 mx-auto text-center"
   ></div>
   <p class="text-center" v-if="message">{{ message }}</p>
 </template>
