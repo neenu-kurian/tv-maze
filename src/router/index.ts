aef7b0af41ace9d62de7b7b75c9db19e8a76e093
neenukurian92@gmail.com <neenukurian92@gmail.com>
2025-10-28 23:18:34 +0100

    feat: add not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ShowDetailView from '../views/ShowDetailView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     component: ShowDetailView,
     name: 'show-detail',
   },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFoundView,
+    name: 'not-found',
+  }
 ];
 
 const router = createRouter({
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-white px-4">
+    <h1 class="text-3xl font-bold text-gray-900 mb-4">Page Not Found</h1>
+    <p class="text-gray-700 mb-6">
+      Oops! The page you are trying to view doesn't exist or has been moved.
+    </p>
+    <div>
+      <RouterLink
+        to="/"
+        class="inline-block bg-gray-100 text-black px-4 py-2 rounded-lg hover:bg-gray-300"
+      >
+        Go Back to Home
+      </RouterLink>
+  </div>
+  </div>
+</template>
+
