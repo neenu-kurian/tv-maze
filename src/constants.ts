fa6056ed40cc1697d8c95ccdaaa9db419768521b
neenukurian92@gmail.com <neenukurian92@gmail.com>
2025-10-29 12:18:47 +0100

    refactor: added constants for timeout and top cast count

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const sortOptions = [
   { value: 'rating-desc', label: 'Rating: High to Low' },
   { value: 'rating-asc', label: 'Rating: Low to High' },
 ];
+export const MAX_CAST_MEMBERS_TO_DISPLAY = 8;
+export const SEARCH_TIMEOUT= 750;
